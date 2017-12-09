a0b591043aa402d018d6ba5f5d5e4e07b10e041c
kikihuaiting <jiaqiwang827@gmail.com>
2017-12-09 14:36:28 -0500

    update

--- src/inmemorydb/InMemoryDB.java
+++ src/inmemorydb/InMemoryDB.java
@@ -1,33 +1,51 @@
 package inmemorydb;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Stack;
+import java.util.*;
 
 public class InMemoryDB {
 	private Map<String, Integer> map = new HashMap<>();
-	private Map<Integer, Integer> countMap = new HashMap<>();
-	private Map<String, Integer> tempMap;
-	private Map<Integer, Integer> tempCountMap = new HashMap<>();
-	private Stack<Map<String, Integer>> stack = new Stack<>();
+	private Map<Integer, Set<String>> countMap = new HashMap<>();
+
+	private Map<Integer, Set<String>> tempCountMap = new HashMap<>();
+	public Stack<Map<String, Integer>> stack = new Stack<>();
 	
 	public void set(String key, Integer value) {
 		if (checkTransaction()) {
-			Map<String, Integer> curTran = stack.peek();
+			Map<String, Integer> curTran = stack.pop();
+			if (curTran.containsKey(key)) {
+				Set<String> set = tempCountMap.get(curTran.get(key));
+				set.remove(key);
+				tempCountMap.put(curTran.get(key), set);				
+			}
 			curTran.put(key, value);
-			if (tempCountMap.containsKey(key)) {
-				tempCountMap.put(value, tempCountMap.get(key) + 1);
+			stack.push(curTran);
+			
+			if (tempCountMap.containsKey(value)) {
+				Set<String> temp = tempCountMap.get(value);
+				temp.add(key);
+				tempCountMap.put(value, temp);
 			} else {
-				tempCountMap.put(value, 1);
+				Set<String> temp = new HashSet<String>();
+				temp.add(key);
+				tempCountMap.put(value, temp);
 			}
 		} else {
+			if (map.containsKey(key)) {
+				Set<String> set = countMap.get(map.get(key));
+				set.remove(key);
+				countMap.put(map.get(key), set);				
+			}
+			
 			map.put(key, value);
+			
 			if (countMap.containsKey(value)) {
-				countMap.put(value, countMap.get(value) + 1);
+				Set<String> temp = countMap.get(value);
+				temp.add(key);
+				countMap.put(value, temp);
 			} else {
-				countMap.put(value, 1);
+				Set<String> temp = new HashSet<String>();
+				temp.add(key);
+				countMap.put(value, temp);
 			}
 		}
 	}
@@ -35,7 +53,13 @@ public class InMemoryDB {
 	public Integer get(String key) {
 		if (checkTransaction()) {
 			Map<String, Integer> curTran = stack.peek();
-			return curTran.containsKey(key) ? curTran.get(key) : null;
+			for (int i = stack.size() - 1; i >= 0; i--) {
+				if (stack.get(i).containsKey(key)) {
+					return stack.get(i).get(key);
+				}
+			}
+			if (map.containsKey(key)) return map.get(key);
+			return null;		
 		} else {
 			return map.containsKey(key) ? map.get(key) : null;			
 		}
@@ -44,11 +68,15 @@ public class InMemoryDB {
 	public void delete(String key) {
 		if (checkTransaction()) {
 			Map<String, Integer> curTran = stack.peek();
-			if (curTran.containsKey(key)) {
-				curTran.remove(key);
-			}
+//			if (curTran.containsKey(key)) {
+//				curTran.remove(key);
+//			}
+			curTran.put("a", null);
 		} else {
-			countMap.put(map.get(key), countMap.get(map.get(key)) -1);
+		//	countMap.put(map.get(key), countMap.get(map.get(key)) -1);
+			Set<String> temp = countMap.get(map.get(key));
+			temp.remove(key);
+			countMap.put(map.get(key), temp);
 			map.remove(key);		
 		}
 	}
@@ -56,24 +84,41 @@ public class InMemoryDB {
 	public int count(Integer value) {
 		int number = 0;
 		if (checkTransaction()) {
-			//TODO
-			number = countMap.get(value) + tempCountMap.get(value);
+			//TODO			
+			if (!countMap.containsKey(value) && !tempCountMap.containsKey(value)) {
+				return 0;
+			}
+			if (countMap.containsKey(value) && tempCountMap.containsKey(value)) {
+				int common = 0;
+				Set<String> set = countMap.get(value);
+				Set<String> tempset = tempCountMap.get(value);
+				for (String s : set) {
+					if (tempset.contains(s)) {
+						common++;
+					}
+				}
+				number = set.size() + tempset.size() - common;				
+			} else if (countMap.containsKey(value)){
+				number = countMap.get(value).size();
+			} else {
+				number = tempCountMap.get(value).size();
+			}
 		} else {
 			//TODO
-			//number = countMap.containsKey(value) ? countMap.get(value).size() : 0;
-			number = countMap.containsKey(value) ? countMap.get(value) : 0;
+			number = countMap.containsKey(value) ? countMap.get(value).size() : 0;
 		}
 		return number;
 	}
 	
 	public void begin() {
-		tempMap = new HashMap();
+		Map<String, Integer> tempMap = new HashMap<String, Integer>();
 		stack.push(tempMap);
 	}
 	
 	public void rollback() {
-		stack.pop();
-		
+		if (!isCommitted()) {
+			stack.pop();			
+		}
 //		for (Map.Entry<String, Integer> entry : tempMap.entrySet()) {
 //			curTran.remove(entry.getKey());
 //		}
@@ -85,23 +130,49 @@ public class InMemoryDB {
 	
 	public void commit() {
 		Stack<Map<String, Integer>> reverseStack = new Stack<>();
+		Set<String> visited = new HashSet<String>();
+		
 		while (!stack.isEmpty()) {
-			reverseStack.add(stack.pop());
+			Map<String, Integer> curTran = stack.pop();
+			//update countMap
+			for (Map.Entry<String, Integer> entry : curTran.entrySet()) {
+				if (!visited.contains(entry.getKey())) {
+					if (countMap.containsKey(entry.getValue())) {
+						Set<String> set = countMap.get(entry.getValue());
+						set.add(entry.getKey());
+						countMap.put(entry.getValue(), set);
+					} else {
+						Set<String> set = new HashSet<>();
+						set.add(entry.getKey());
+						countMap.put(entry.getValue(), set);			
+					}
+					visited.add(entry.getKey());
+				}
+			}
+			reverseStack.add(curTran);
 		}
+		
 		while (!reverseStack.isEmpty()) {
 			Map<String, Integer> curTran = reverseStack.pop();
+			
 			for (Map.Entry<String, Integer> entry : curTran.entrySet()) {
 				map.put(entry.getKey(), entry.getValue());
 			}
-		}
+		}		
 	}
 	
-	private boolean checkTransaction() {
-		for (Map<String,Integer> m : stack) {
-			if (!m.isEmpty()) {
-				return true;
-			}	
-		}
-		return false;
+	public boolean checkTransaction() {
+//		if (!stack.isEmpty()) {
+//			for (Map<String,Integer> m : stack) {
+//				if (m.size() != 0) {
+//					return true;
+//				}	
+//			}			
+//		}
+		return stack.isEmpty() ? false : true;
+	}
+	
+	private boolean isCommitted() {
+		return stack.isEmpty() ? true : false;
 	}
 }
--- src/test/InMemoryDBTest.java
+++ src/test/InMemoryDBTest.java
@@ -12,54 +12,84 @@ class InMemoryDBTest {
 	
 	@Test
 	public void test() {
-//		tc.set("a", 10);
-//		int ia = tc.get("a");
-//		assertEquals(ia, 10);
-//		tc.delete("a");
-//		Integer ia2 = tc.get("a");
-//		assertEquals(ia2, null);
-		
-		tc.begin();
 		tc.set("a", 10);
-		int ia3 = tc.get("a");
-		assertEquals(ia3, 10);
+		int ia = tc.get("a");
+		assertEquals(ia, 10);
+		System.out.println(tc.count(10));
+		
+		tc.delete("a");
+		Integer ia2 = tc.get("a");
+		assertEquals(ia2, null);
+		System.out.println(tc.count(10));
+	
 		tc.begin();
 		tc.set("a", 20);
+		int ia3 = tc.get("a");
+		assertEquals(ia3, 20);
+		System.out.println(tc.count(20));
+		System.out.println(tc.count(10));
+
+		tc.begin();
+		tc.set("a", 30);
 		int kar = tc.get("a");
-		assertEquals(kar, 20);
+		assertEquals(kar, 30);
+		System.out.println(tc.count(30));
+
 		tc.rollback();
 		
 		int ia4 = tc.get("a");
 		assertEquals(ia4, 20);
-		
+		System.out.println(tc.count(20));
+	
 		tc.rollback();
-		int ia5 = tc.get("a");
-		assertEquals(ia5, 10);
+		Integer ia5 = tc.get("a");
+		assertEquals(ia5, null);
+		System.out.println(tc.count(20));
 		
-//		
-//		tc.begin();
-//		tc.set("a", 30);
-//		tc.begin();
-//		tc.set("a", 40);
-//		tc.commit();
-//		int ia = tc.get("a");
-//		assertEquals(ia, 40);
-		
-		
+		tc.begin();
+		tc.set("a", 30);
+		tc.begin();
+		tc.set("a", 40);
+		tc.commit();
+		int ia6 = tc.get("a");
+		assertEquals(ia6, 40);
+		System.out.println(tc.count(40));
+		System.out.println(tc.count(30));
+
+		tc.rollback();
+		int ia7 = tc.get("a");
+		assertEquals(ia7, 40);
+	
 		tc.set("a", 50);
-		
+		tc.set("b", 60);
+		tc.set("c", 60);
+		tc.set("d", 50);
 		tc.begin();
 		int ia8 = tc.get("a");
 		assertEquals(ia8, 50);
 		tc.set("a", 60);
 		tc.begin();
-		tc.delete("a");
-		Integer ia6 = tc.get("a");
-		assertEquals(ia6, null);
+		tc.set("a", 40);
+ 		System.out.println(tc.count(50));
+		System.out.println(tc.count(60));
+		System.out.println(tc.count(40));
+//
+//
+//		tc.begin();
+//		tc.set("b", 1);
+//		tc.delete("a");
+//     	System.out.println(tc.count(60));
 		
+//		System.out.println(tc.checkTransaction());		
+//
+//		Integer ia9 = tc.get("a");
+//		assertEquals(ia9, null);
+//		int ib = tc.get("b");
+//		assertEquals(ib, 1);
 //		tc.rollback();
-//		int ia7 = tc.get("a");
-//		assertEquals(ia7, 60);
+//	//	System.out.println(tc.get("a"));
+//		int ia10 = tc.get("a");
+//		assertEquals(ia10, 60);
 	}	
 }
 
